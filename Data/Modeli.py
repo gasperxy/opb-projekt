fb62768a320de26fcf1f95bf312aa089deea0981
gasper <gasper.romih@netica.si>
2023-04-29 16:22:03 +0200

    dodane genericne metode in primeri

--- Data/Modeli.py
+++ Data/Modeli.py
@@ -1,15 +1,22 @@
 from dataclasses import dataclass, field
+from dataclasses_json import dataclass_json
 
 # Predlagam uporabo (vsaj) dataclass-ov
 
 # Nahitro dataclass doda nekaj bližnjic za delo z razredi in omogoča tudi nekaj bolj naprednih funkcij,
 # ki jih načeloma najdemo v bolj tipiziranih jezik kot so C#, Java, C++,..
 
+# Knjižnjica dataclasses_json je nadgradnja za delo z dataclasses in omogoča predvsem
+# preprosto serializacijo in deseralizacijo objektov. Poleg tega vsebuje tudi uporabno funkcijo
+# to_dict() in from_dict(), ki dataclas pretvori v/iz slovarja.
+@dataclass_json
 @dataclass
 class Izdelek:
     id: int = field(default=0)
     ime: str = field(default="")
     kategorija: int = field(default=0)
+
+
 
 @dataclass
 class IzdelekDto:
@@ -18,12 +25,13 @@ class IzdelekDto:
     oznaka: str = field(default="")
 
 
-
+@dataclass_json
 @dataclass
 class KategorijaIzdelka:
     id: int = field(default=0)
     oznaka: str = field(default="")
-
+    
+@dataclass_json
 @dataclass
 class CenaIzdelka:
     id: int = field(default=0)
--- Data/uvozi.py
+++ Data/uvozi.py
@@ -1,11 +1,25 @@
 import pandas as pd
+from pandas import DataFrame
 
 from Data.Database import Repo
 from Data.Modeli import *
+from typing import Dict
+from re import sub
+import dataclasses
 
+
+# Vse kar delamo z bazo se nahaja v razredu Repo.
 repo = Repo()
 
+
+
 def uvozi_cene(pot):
+    """
+    Uvozimo csv s cenami izdelkov v bazo.
+    Pri tem predpostavljamo, da imamo tabele že ustvarjene. 
+    Vsako vrstico posebaj še obdelamo, da v bazi dobimo željeno strukturo. Uporabimo
+    razred Repo za klic funkcij za uvoz v bazo.
+    """
 
     df = pd.read_csv(pot, sep=";",skiprows=[0], encoding="Windows-1250")
 
@@ -23,6 +37,8 @@ def uvozi_cene(pot):
        
         if row[1].startswith("..."):
             # Vrstica je izdelek
+
+
             izdelek = repo.dodaj_izdelek(
                 Izdelek(
                  ime=row[1].replace("...", ""),
@@ -53,14 +69,91 @@ def uvozi_cene(pot):
             )
 
 
-  
+def uvozi_csv(pot, ime):
+    """
+    Uvozimo csv v bazo brez večjih posegov v podatke.
+    Ustvarimo pandasov DataFrame ter nato z generično metodo ustvarimo ter
+    napolnimo tabelo v postgresql bazi.
+    """
+    df = pd.read_csv(pot, sep=";",skiprows=[0], encoding="Windows-1250")
 
+    # Zamenjamo pomišljaje z prazno vrednostjo
+    df.replace(to_replace="-", value="", inplace=True)
+
+    # Naredimo tabelo z dodatnim serial primary key stolpcem
+    repo.df_to_sql_create(df, ime, add_serial=True, use_camel_case=True)
+
+    # uvozimo podatke v to isto tabelo
+    repo.df_to_sql_insert(df, ime, use_camel_case=True)
+
+
+
+
+# Primeri uporabe. Zakomentiraj določene vrstice, če jih ne želiš izvajat!
     
 
+pot = "Data/cene.csv"
 
 
 
-#uvozi_cene("Uvoz/cene.csv")
+# Uvozi csv s cenami izdelkov v ločene (in povezane) entitete
+# Tabele morajo biti prej ustvarjene, da zadeva deluje
 
-for cena in repo.cena_izdelkov():
-    print(cena)
+# uvozi_cene(pot)
+
+# Uvozi csv s cenami, le da tokar uvozi le eno tabelo, ki jo
+# predhodno še ustvari, če ne obstaja.
+
+# uvozi_csv(pot, "NovaTabela")
+
+
+
+
+# S pomočjo generične metode dobimo seznam izdelkov in kategorij
+# Privzete nastavi
+
+# Dobimo prvih 100 izdelkov
+izdelki = repo.dobi_gen(Izdelek, skip=0, take=100)
+
+t = repo.dobi_gen(IzdelekDto)
+
+# Dobimo prvih 10 kategorij
+kategorije = repo.dobi_gen(KategorijaIzdelka)
+
+# Dodamo novo kategorijo
+
+nova_kategorija = KategorijaIzdelka(
+    oznaka="Nova kategorija"
+)
+
+repo.dodaj_gen(nova_kategorija)
+
+# vrednost nova_kategorija.id je sedaj določen na podlagi
+# serial vrednosti iz baze in jo lahko uporabimo naprej.
+
+
+# Dodamo nov izdelek v to kategorijo
+novi_izdelek = Izdelek(
+    ime = 'Novi izdelek',
+    kategorija=nova_kategorija.id
+)
+repo.dodaj_gen(novi_izdelek)
+
+
+# Dobimo izdelek z idjem 832
+izdelek = repo.dobi_gen_id(Izdelek, 832)
+
+# izdelku spremenimo ime in ga posodobimo v bazi
+izdelek.ime += " spremenjeno ime"
+repo.posodobi_gen(izdelek)
+
+
+
+# spremenimo seznam izdelkov in ga shranimo v bazo
+
+for i in izdelki:
+    i.ime = f'({i.ime})'
+
+repo.posodobi_list_gen(izdelki)
+
+
